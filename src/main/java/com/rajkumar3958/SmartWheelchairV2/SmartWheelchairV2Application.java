71deec9485ad523690e2831eff2132d2a22a05be
Raj Kumar <rajk64656@gmail.com>
2023-05-20 18:50:40 +0530

    inital spring boot code

---
+++ src/main/java/com/rajkumar3958/SmartWheelchairV2/SmartWheelchairV2Application.java
@@ -0,0 +1,13 @@
+package com.rajkumar3958.SmartWheelchairV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartWheelchairV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartWheelchairV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rajkumar3958/SmartWheelchairV2/SmartWheelchairV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajkumar3958.SmartWheelchairV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartWheelchairV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
